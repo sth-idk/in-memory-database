cdded0fc703db717c0ed901bf0e5eaac31431e8d
Farya <farya.hemati1313@gmail.com>
2025-04-01 16:32:29 +0330

    update the Database class.

--- src/db/Database.java
+++ src/db/Database.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<Entity>();
+    static boolean toCheck = true;
 
     private static void add(Entity e){
         entities.add(e);
@@ -15,27 +16,39 @@ public class Database {
 
     private static Entity get(int id){
         for(Entity entity : entities){
-            if(entity.id == id)
+            if(entity.id == id) {
+                toCheck = false;
                 return entity;
+            }
+        }
+        if(toCheck){
+            throw new EntityNotFoundException(id);
         }
         return null;
     }
+
     public static Entity callGet(int id){
         return Database.get(id);
     }
 
+
     private static void delete(int id){
         for(Entity entity : entities){
-            if(entity.id == id)
+            if(entity.id == id) {
                 entities.remove(entity);
+                toCheck = false;
+            }
         }
+            if(toCheck)
+                throw new EntityNotFoundException(id);
     }
+
     public static void callDelete(int id){
         Database.delete(id);
     }
 
+
     private static void update(Entity e){
-        boolean toCheck = true;
         for(Entity entity : entities){
             if(entity.id == e.id){
                 int index = entities.indexOf(entity);
@@ -43,7 +56,10 @@ public class Database {
                 toCheck = false;
             }
         }
+        if(toCheck)
+            throw new EntityNotFoundException();
     }
+
     public static void callUpdate(Entity e){
         Database.update(e);
     }
