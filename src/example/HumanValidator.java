fe00d03e06d5aa22e370a17ec9a4581e182d64e9
Farya <farya.hemati1313@gmail.com>
2025-04-09 13:25:26 +0330

    add one abstract method named getEntityCode to the Entity class.

---
+++ src/example/HumanValidator.java
@@ -0,0 +1,14 @@
+package example;
+
+import db.Entity;
+import db.Validator;
+import db.exception.InvalidEnitityException;
+
+public class HumanValidator implements Validator {
+
+    @Override
+    public void validate(Entity entity) throws InvalidEnitityException{
+
+
+    }
+}
