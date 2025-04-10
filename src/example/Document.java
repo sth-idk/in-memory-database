1cf6bf88556711a4a603f8d343ef0e0330cc6c4f
Farya <farya.hemati1313@gmail.com>
2025-04-10 23:15:52 +0330

    implement the methods to the Document class.

--- src/example/Document.java
+++ src/example/Document.java
@@ -12,5 +12,30 @@ public class Document extends Entity implements Trackable {
         this.content = content;
     }
 
+    @Override
+    public int getEntityCode() {
+        return 0;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return null;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return null;
+    }
+
 
 }
