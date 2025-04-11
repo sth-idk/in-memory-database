606647d5622cb502412772d46e140aa3653a5855
Farya <farya.hemati1313@gmail.com>
2025-04-11 19:41:54 +0330

    add one class named StepService to todo.sevice package.

---
+++ src/todo/service/StepService.java
@@ -0,0 +1,10 @@
+package todo.service;
+
+public class StepService {
+
+        public static void saveStep(int taskRef, String title) {
+            // Create a new step with the specified taskRef and title, then
+            // set it state to NotStarted and save it to the database
+        }
+    }
+}
