3599667d64e1175cabcd68e9956b9a0f1e218532
Farya <farya.hemati1313@gmail.com>
2025-04-11 17:48:35 +0330

    add one string field named dueDate to Task class.

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -6,6 +6,7 @@ import db.Trackable;
 public class Task extends Entity implements Trackable {
     String title;
     String description;
+    String dueDate;
     enum Status{
         NotStarted,
         InProgress,
