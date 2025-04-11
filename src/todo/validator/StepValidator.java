a7172ba3978702430139755d3e895e4ad0d4c458
Farya <farya.hemati1313@gmail.com>
2025-04-11 18:19:44 +0330

    add one class implementing Validator, named StepValidator, to todo.validator package.

---
+++ src/todo/validator/StepValidator.java
@@ -0,0 +1,12 @@
+package todo.validator;
+
+import db.Entity;
+import db.Validator;
+import db.exception.InvalidEnitityException;
+
+public class StepValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEnitityException {
+
+    }
+}
