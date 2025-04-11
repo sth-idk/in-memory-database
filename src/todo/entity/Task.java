26700a932679609b10e4e100323a4f5a6b2361b3
Farya <farya.hemati1313@gmail.com>
2025-04-11 17:51:34 +0330

    add one Status field named status to Task class.

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -9,7 +9,7 @@ public class Task extends Entity implements Trackable {
     String title;
     String description;
     Date dueDate;
-    String status;
+    Status status;
     enum Status{
         NotStarted,
         InProgress,
